83dfbcef97e9130ff6d85e16f29a44aed23b6bcb
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-02-28 01:10:35 -0500

    Cleaning code - faster solution

--- 0. CodeForces/jojojo_feliz_navidad.cpp
+++ 0. CodeForces/jojojo_feliz_navidad.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <string>
-#include <stdlib.h>
-#include <utility>
 #include <vector>
 
 using namespace std;
@@ -9,12 +7,8 @@ using namespace std;
 #define FORN(i,m,n) for(int i=(m); i<int(n); i++)
 
 int y;
-
-string solve(vector<char> actions){
-    int count_b{0}, count_m{0};
-    FORN(i,0,y) actions[i]=='B' ? count_b++ : count_m++;
-    return (2*count_m < count_b) ? "REGALO" : "CARBON"; 
-}
+const int ms = 1e3+5;
+vector<char> actions(ms);
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL);
@@ -22,10 +16,13 @@ int main(){
     cin>>n;
     FORN(i,0,n){
         string name;
+        int count_b{0};
         cin>>name;
         cin>>y;
-        vector<char> actions(y);
-        FORN(j,0,y) cin>>actions[j];
-        cout<<name<<" "<<solve(actions)<<endl;
+        FORN(j,0,y){
+            cin>>actions[j];
+            if(actions[j]=='B') count_b++;
+        }
+        cout<<name<<(count_b > 2*(y-count_b) ? " REGALO" : " CARBON")<<"\n";
     }
 }
