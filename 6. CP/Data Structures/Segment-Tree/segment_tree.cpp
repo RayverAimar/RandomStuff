7a67a54f06e7de97e25417869f129a2e0fe9822d
Rayver Aimar <rayver.munoz@ucsp.edu.pe>
2022-04-15 12:30:15 -0500

    Add queries function

--- 6. CP/Data Structures/Segment-Tree/segment_tree.cpp
+++ 6. CP/Data Structures/Segment-Tree/segment_tree.cpp
@@ -27,7 +27,7 @@ typedef vector<vl> vvl;
 
 const int ms = 1e5+5;
 
-const int n=9;
+const int n=10;
 int s_tree[ms<<1], nodes[ms];
 
 void build(int cur_node, int s, int e){
@@ -40,14 +40,30 @@ void build(int cur_node, int s, int e){
     }
 }
 
+void update(int u, int v){
+    for(s_tree[u+=n]=v; u>1; u>>=1)
+        s_tree[u>>1] = max(s_tree[u], s_tree[u+1]);
+}
+
+int query(int cur_node, int lBound, int rBound, int lQ, int rQ) {
+    if (lQ > rBound || rQ < lBound) return -1; // No overlap
+    if (lBound >= lQ && rBound <= rQ) return s_tree[cur_node]; // Total Overlap
+    int l = query(cur_node<<1, lBound , (lBound + rBound)>>1, lQ, rQ); //Partial Overlap
+    int r = query(cur_node<<1|1, ((lBound + rBound)>>1)+1, rBound , lQ, rQ); //Partial Overlap
+    if (l == -1) return r; 
+    if (r == -1) return l;
+    return max(l,r);
+}
+
 void print(){
-    FORN(i,1,18) cout<< s_tree[i] <<" ";
+    FORN(i,1,n*4) cout<< s_tree[i] <<" ";
 }
 
 int main(){
     //ios_base::sync_with_stdio(false); cin.tie(NULL);
-    int B[n] = {1,5,6,2,4,16,21,15,67};
+    int B[n] = {-1,-1,1,1,1,1,3,10,10,10};
     FORN(i,0,n) nodes[i]=B[i];
     build(1, 0, n-1);
+    cout<<query(1,0,n-1,2,6)<<endl;
     print();
 }
